d7fea588b472011c76ce8c1cf91c0b263e8e3a0b
Rob Hammond <rhammond@brightedge.com>
2022-10-13 13:33:33 +0100

    🚘 Add car efficiency stats

--- app/web/routes/garage.js
+++ app/web/routes/garage.js
@@ -31,8 +31,44 @@ router.get("/", async (req, res) => {
         cs.carId = max_date.carId 
 	    AND cs.datetime = max_date.datetime
     `;
+    carEfficiency = [];
+    for (let car of cars) {
+        let car_id = car["carId"];
+        let efficiency = await prisma.$queryRaw`
+            WITH batt AS (
+                SELECT
+                    strftime("%Y-%m-%d", cs.datetime, 'localtime') AS datetime,
+                    c.id AS car_id,
+                    c.batterySize,
+                    cs.batteryPercent,
+                    ROUND(CASE
+                        WHEN odometerUnit = 'km' THEN ((odometer * 0.62137) - LAG((odometer * 0.62137), 1) OVER (ORDER BY datetime))
+                        ELSE (odometer - LAG(odometer, 1) OVER (ORDER BY datetime))
+                    END, 0) AS distance_travelled,
+                    ROUND( (batteryPercent  - LAG(batteryPercent, 1) OVER (ORDER BY datetime)), 0) * -1 AS battery_percent_used
+                FROM CarStatus cs
+                JOIN Car c ON
+                    c.id = cs.carId
+                WHERE
+                    c.id = ${car_id}
+                GROUP BY 1,2,3,4
+                ORDER BY cs.datetime DESC
+            )
+            SELECT b.datetime,
+                c.make || ' ' || c.model AS carName,
+                SUM(b.distance_travelled) AS distance_travelled,
+                b.batterySize * (SUM(battery_percent_used)/100) AS kwh_used,
+                ROUND(SUM(b.distance_travelled) / (b.batterySize * (SUM(battery_percent_used)/100)), 2) AS mpkwh
+            FROM batt b
+            LEFT JOIN Car c ON c.id = b.car_id
+            WHERE battery_percent_used > 0
+            GROUP BY 1,2
+            ORDER BY 1
+        `;
+        carEfficiency.push(efficiency);
+    }
 
-    res.render("garage", { page_title: "Garage", cars: cars });
+    res.render("garage", { page_title: "Garage", cars: cars, carEfficiency: carEfficiency });
 });
 
 router.get("/car", async (req, res) => {
@@ -72,7 +108,7 @@ router.get("/car", async (req, res) => {
             cs.carId = ${Number(id)}
             AND cs.datetime = max_date.datetime
     `;
-    
+
     const carTrips = await prisma.$queryRaw`
         SELECT
             strftime("%Y-%m-%d", cs.datetime, 'localtime') AS datetime,
@@ -95,14 +131,44 @@ router.get("/car", async (req, res) => {
     if (car.dateAcquired) {
         var end = DateTime.now();
         var start = DateTime.fromISO(car.dateAcquired);
-        monthsOwned = end.diff(start, ['months', 'days']).toObject();
+        monthsOwned = end.diff(start, ["months", "days"]).toObject();
     }
+
+    let carEfficiency = await prisma.$queryRaw`
+        WITH batt AS (                    
+            SELECT
+                strftime("%Y-%m-%d", cs.datetime, 'localtime') AS datetime,
+                c.batterySize,
+                cs.batteryPercent,
+                ROUND(CASE
+                    WHEN odometerUnit = 'km' THEN ((odometer * 0.62137) - LAG((odometer * 0.62137), 1) OVER (ORDER BY datetime))
+                    ELSE (odometer - LAG(odometer, 1) OVER (ORDER BY datetime))
+                END, 0) AS distance_travelled,
+                ROUND( (batteryPercent  - LAG(batteryPercent, 1) OVER (ORDER BY datetime)), 0) * -1 AS battery_percent_used
+            FROM CarStatus cs
+            JOIN Car c ON
+                c.id = cs.carId
+            WHERE
+                cs.carId = ${Number(id)}
+            GROUP BY 1,2,3
+            ORDER BY cs.datetime DESC
+        )
+        SELECT b.datetime,
+            SUM(b.distance_travelled) AS distance_travelled,
+            batterySize * (SUM(battery_percent_used)/100) AS kwh_used,
+            ROUND(SUM(b.distance_travelled) / (batterySize * (SUM(battery_percent_used)/100)), 2) AS mpkwh
+        FROM batt b
+        WHERE battery_percent_used > 0
+        GROUP BY 1
+        ORDER BY 1 DESC
+    `;
 
     res.render("garage/car", {
         page_title: `${car.make} ${car.model}`,
         car: car,
         carStatus: carStatus[0],
         carTrips: carTrips,
+        carEfficiency: carEfficiency,
         monthsOwned: monthsOwned,
     });
 });
