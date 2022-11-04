efca244fa20b55370794194c30a694237f35767f
Rob Hammond <rhammond@brightedge.com>
2022-11-04 13:19:42 +0000

    💅 Nicer totals display for energy stats

--- app/web/public/js/tables.js
+++ app/web/public/js/tables.js
@@ -11,13 +11,41 @@ function loadUsageTable(unit, startDate, endDate, filter) {
         dataType: "json",
         success: function(res) {
             reversed_array = res.data.reverse();
+            let count = 0;
             for (let row of reversed_array) {
                 $('#usageDataTable').append(`<tr><td>${row.dt}</td><td style="text-align:right">${row.kwh}</td>
                     <td style="text-align:right">£${row.cost.toFixed(2)}</td></tr>`);
+                count++;
             }
             let totals = res.totals;
-            $('#totalkWh').html(`${totals["kwh"].toFixed(0).toLocaleString()} kWh`);
-            $('#totalCost').html(`£${totals["cost"].toFixed(2).toLocaleString()}`);
+            $('#totalkWh').html(`
+                <div class="card border-info mb-3" style="max-width: 18rem;">
+                <div class="card-header">Total kWh</div>
+                <div class="card-body">
+                    <h5 class="card-title" style="text-align:right;">${ Number(totals["kwh"].toFixed(2)).toLocaleString() }</h5>
+                </div>
+            </div>`);
+            $('#totalCost').html(`
+            <div class="card border-info mb-3" style="max-width: 18rem;">
+                <div class="card-header">Total Cost</div>
+                <div class="card-body">
+                    <h5 class="card-title" style="text-align:right;">&pound;${ Number(totals["cost"].toFixed(2)).toLocaleString() }</h5>
+                </div>
+            </div>`);
+            $('#avgkWh').html(`
+                <div class="card border-info mb-3" style="max-width: 18rem;">
+                <div class="card-header">Average kWh</div>
+                <div class="card-body">
+                    <h5 class="card-title" style="text-align:right;">${ Number((totals["kwh"] / count).toFixed(2)).toLocaleString() }</h5>
+                </div>
+            </div>`);
+            $('#avgCost').html(`
+            <div class="card border-info mb-3" style="max-width: 18rem;">
+                <div class="card-header">Average Cost</div>
+                <div class="card-body">
+                    <h5 class="card-title" style="text-align:right;">&pound;${ Number((totals["cost"] / count).toFixed(2)).toLocaleString() }</h5>
+                </div>
+            </div>`);
         },
         error: function(xhr) {
             console.log(xhr);
